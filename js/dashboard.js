3e661560f045d2ac0378c65928cee082a464d70d
Pawan <pawan@getupdates.com>
2020-08-18 23:35:54 +0530

    Table Fix

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,11 @@
 $(document)
     .ready(function ($) {
+
+        $('#datetimepicker1').datetimepicker({
+            format: 'DD/MM/YYYY HH:mm'
+        });
+
+
         $(".expand-collapse-table-rows")
             .on("click", function () {
                 $(this)
@@ -8,4 +14,9 @@ $(document)
 
                 $(".table-child-" + $(this).data('item')).toggleClass('d-none')
             })
+
+        $(".schedule-icon").on('click', function () {
+            //$(".schedule-options").addClass('d-none');
+            $(this).siblings(".schedule-options").toggleClass('d-flex')
+        })
     })
