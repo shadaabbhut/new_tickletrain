8a199621596a166b5d563689ffb2d071e2d936be
Pawan <pawan@getupdates.com>
2020-08-23 19:41:50 +0530

    dashboard for user info

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,9 @@
 $(document)
     .ready(function ($) {
-
-        $('#datetimepicker1').datetimepicker({
-            format: 'DD/MM/YYYY HH:mm'
-        });
-
+        if ($.fn.datepicker)
+            $('#datetimepicker1').datetimepicker({
+                format: 'DD/MM/YYYY HH:mm'
+            });
 
         $(".expand-collapse-table-rows")
             .on("click", function () {
@@ -17,6 +16,16 @@ $(document)
 
         $(".schedule-icon").on('click', function () {
             //$(".schedule-options").addClass('d-none');
-            $(this).siblings(".schedule-options").toggleClass('d-flex')
+            $(this)
+                .siblings(".schedule-options")
+                .toggleClass('d-flex')
         })
+
+        $('.is-floating-label input, .is-floating-label textarea').on('focus blur', function (e) {
+            $(this).parents('.is-floating-label').toggleClass('is-focused', (e.type === 'focus' || this.value.length > 0));
+        }).trigger('blur');
+
+        setTimeout(function () {
+
+        }, 4000)
     })
